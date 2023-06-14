3ff19763e20d8bce975496b5eadfc4e246b955bd
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-06-14 14:00:01 +0300

    feat: add 'filters_block' mixin

--- sources/src/js/catalog.js
+++ sources/src/js/catalog.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // INIT CATALOG
 function initCatalog() {
-  // add listeners to catalog__categories
+  // handle accordions in catalog__categories
   const categories_titles = document.querySelectorAll(".categories__title");
   const categories_groups = document.querySelectorAll(".categories__group");
 
@@ -34,6 +34,69 @@ function initCatalog() {
       });
       group.classList.add("active");
       list.style.maxHeight = list.scrollHeight + "px";
+    }
+  }
+
+  // handle all input range in filters
+
+  const filters_range_inputs = document.querySelectorAll(".filters__range");
+
+  filters_range_inputs.forEach((rangeInput) =>
+    rangeInput.addEventListener("input", rangeInputHandler)
+  );
+
+  function rangeInputHandler(e) {
+    let minInputRange, maxInputRange;
+    const isMinInputRange = e.target.classList.contains("filter__inputMin");
+    const isMaxInputRange = e.target.classList.contains("filter__inputMax");
+    if (isMinInputRange) {
+      minInputRange = e.target;
+      maxInputRange = minInputRange.nextElementSibling;
+    } else if (isMaxInputRange) {
+      maxInputRange = e.target;
+      minInputRange = maxInputRange.previousElementSibling;
+    }
+
+    const filter_block = minInputRange.closest(".filters__block");
+    const track = filter_block.querySelector(".filters__track");
+    const min = filter_block.querySelector(".filters__valueMin span");
+    const max = filter_block.querySelector(".filters__valueMax span");
+
+    let minValue = parseInt(minInputRange.value);
+    let maxValue = parseInt(maxInputRange.value);
+
+    let minDifference;
+
+    if (maxInputRange.max - minInputRange.min <= 10) {
+      minDifference = 1;
+    } else if (maxInputRange.max - minInputRange.min <= 100) {
+      minDifference = 3;
+    } else {
+      minDifference = 10;
+    }
+
+    if (maxValue - minValue < minDifference) {
+      if (isMinInputRange) {
+        minInputRange.value = maxValue - minDifference;
+      } else {
+        maxInputRange.value = minValue + minDifference;
+      }
+    } else {
+      min.textContent = minValue;
+      max.textContent = maxValue;
+      let left =
+        ((minValue - minInputRange.min) * 100) /
+          (minInputRange.max - minInputRange.min) +
+        "%";
+      let right =
+        ((maxInputRange.max - maxValue) * 100) /
+          (maxInputRange.max - maxInputRange.min) +
+        "%";
+      track.style.left = left;
+      track.style.right = right;
+
+      console.log("left: ", left);
+      console.log("right: ", right);
     }
   }
 }
